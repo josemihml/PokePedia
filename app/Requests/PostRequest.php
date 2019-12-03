c73ad57c09ff3c2063ac772ad9702d0707d68e54
josemihml <50922763+josemihml@users.noreply.github.com>
2019-12-03 12:48:37 +0100

    Add files via upload

--- app/Requests/PostRequest.php
+++ app/Requests/PostRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class PokemonRequest extends FormRequest
+class PostRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -23,10 +23,10 @@ class PokemonRequest extends FormRequest
      */
     public function rules()
     {
-        return [
-            'name'        => 'required| min:1 | max:100 |unique:pokemon',
-            'weight'       => 'required| numeric ',
-            'height'      => 'required| numeric ',
+        return[
+            'subject'            =>  'required | min:1 | max:200 ',
+            'content'            =>  'required | min:1 | max:300' 
+            
         ];
     }
 }
